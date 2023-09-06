49aaea75a054422ae14abf919b7c5c83271809f9
unknown <jenha04@gmail.com>
2023-09-06 17:32:26 -0500

    Primer commit con Auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {useAuth0} from '@auth0/auth0-react'
 
 function App() {
+
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Aplicación</h1>
+      <button onClick={() => loginWithRedirect()}>Login</button>
     </div>
   );
 }
